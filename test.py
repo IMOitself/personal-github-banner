95ed6b4b0dfa3bc81f02fb0595c306537a0bbe71
IMO <180146096+IMOitself@users.noreply.github.com>
2026-03-12 00:39:02 +0800

    feat: get raw data for commit count on own and other's repo

--- test.py
+++ test.py
@@ -10,13 +10,20 @@ if(TOKEN is None):
     print("access token is None")
     exit()
 
-def query_graphql(query, variables=None):
+def query_graphql(query, variables={}):
     headers = {"Authorization": f"Bearer {TOKEN}"}
     URL = "https://api.github.com/graphql"
-    if(variables is None):
-        variables = {}
     response = requests.post(URL, json={'query': query, 'variables': variables }, headers=headers)
     return response.json()
 
-query = Path('graphql/test.graphql').read_text()
-print(query_graphql(query, {"number_of_repos": 3}))
+query = "query { viewer { id } }"
+output = query_graphql(query)
+viewerId = eval(str(output))['data']['viewer']['id']
+
+query = Path('graphql/commits-on-own-repo.graphql').read_text()
+output = query_graphql(query, {"viewerId": viewerId})
+print(output)
+
+query = Path('graphql/commits-on-other-repo.graphql').read_text()
+output = query_graphql(query, {"viewerId": viewerId})
+print(output)
