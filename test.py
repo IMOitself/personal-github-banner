8316513336352548a4a5059ce1b412e27110fb78
IMO <180146096+IMOitself@users.noreply.github.com>
2026-03-11 17:40:50 +0800

    chore: add graphql file for studying github api

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from dotenv import load_dotenv
 from pathlib import Path
 load_dotenv()
@@ -12,9 +13,13 @@ if(TOKEN is None):
 def query_graphql(query, variables=None):
     headers = {"Authorization": f"Bearer {TOKEN}"}
     URL = "https://api.github.com/graphql"
+    if(variables is None):
+        variables = {}
     response = requests.post(URL, json={'query': query, 'variables': variables }, headers=headers)
     return response.json()
 
 query = Path('graphql/test.graphql').read_text()
-
 print(query_graphql(query, {"number_of_repos": 3}))
+
+query = Path('graphql/help.graphql').read_text()
+Path("output.json").write_text(json.dumps(query_graphql(query), indent=2))
