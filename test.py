aa1cdc42f12decab3e9f824217a56b4e60de39d1
IMO <180146096+IMOitself@users.noreply.github.com>
2026-03-12 16:35:32 +0800

    chore: merge code repetitions

--- test.py
+++ test.py
@@ -21,29 +21,22 @@ overall_commits = 0
 # get github id
 query = "query { viewer { id } }"
 output = query_graphql(query)
-viewerId = eval(str(output))['data']['viewer']['id']
+viewerId = asDict(output)['data']['viewer']['id']
 
-# commit count on own repo
-query = Path('graphql/commits-on-own-repo.graphql').read_text()
-output = query_graphql(query, {"viewerId": viewerId})
+commit_count_strings = [
+    ('graphql/commits-on-own-repo.graphql', 'repositories'),
+    ('graphql/commits-on-other-repo.graphql', 'repositoriesContributedTo')
+]
 
-repos = eval(str(output))['data']['viewer']['repositories']['nodes']
+for graphql_path, repo_tag in commit_count_strings:
+    query = Path(graphql_path).read_text()
+    output = query_graphql(query, {"viewerId": viewerId})
+    repos = asDict(output)['data']['viewer'][repo_tag]['nodes']
 
-for repo in repos:
-    name = eval(str(repo))['nameWithOwner']
-    total_count = eval(str(repo))['defaultBranchRef']['target']['history']['totalCount']
-    print(total_count, name)
-    overall_commits += int(total_count)
-
-# commits count on other people's repo
-query = Path('graphql/commits-on-other-repo.graphql').read_text()
-output = query_graphql(query, {"viewerId": viewerId})
-repos = eval(str(output))['data']['viewer']['repositoriesContributedTo']['nodes']
-
-for repo in repos:
-    name = eval(str(repo))['nameWithOwner']
-    total_count = eval(str(repo))['defaultBranchRef']['target']['history']['totalCount']
-    print(total_count, name)
-    overall_commits += int(total_count)
+    for repo in repos:
+        name = asDict(repo)['nameWithOwner']
+        total_count = asDict(repo)['defaultBranchRef']['target']['history']['totalCount']
+        print(total_count, name)
+        overall_commits += int(total_count)
 
 print("OVERALL:", overall_commits)
