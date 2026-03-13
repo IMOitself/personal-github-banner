10dbb85e3d6ee424e3a62ea10f13f06daf6399ef
IMO <180146096+IMOitself@users.noreply.github.com>
2026-03-13 23:31:42 +0800

    feat(scripts): get and print overall commits

--- get_data.py
+++ get_data.py
@@ -1,4 +1,42 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+from pathlib import Path
+
 class GetData:
-    @staticmethod
-    def test():
-        return "pretend we got data from github api :D"
+    def __init__(self):
+        load_dotenv()
+        self.TOKEN = os.getenv("ACCESS_TOKEN")
+        self.viewerId = self.get_viewer_id()
+
+    def query_graphql(self, query, variables={}):
+        headers = {"Authorization": f"Bearer {self.TOKEN}"}
+        URL = "https://api.github.com/graphql"
+        response = requests.post(URL, json={'query': query, 'variables': variables }, headers=headers)
+        return eval(str(response.json()))
+    # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+    
+    def get_viewer_id(self):
+        query = "query { viewer { id } }"
+        return self.query_graphql(query)['data']['viewer']['id']
+    
+    def get_overall_commits(self):
+        overall_commits = 0
+
+        commit_count_strings = [
+            ('graphql/commits-on-own-repo.graphql', 'repositories'),
+            ('graphql/commits-on-other-repo.graphql', 'repositoriesContributedTo')
+        ]
+
+        for graphql_path, repo_tag in commit_count_strings:
+            query = Path(graphql_path).read_text()
+            repos = self.query_graphql(query, {"viewerId": self.viewerId})['data']['viewer'][repo_tag]['nodes']
+
+            for repo in repos:
+                name = repo['nameWithOwner']
+                total_count = repo['defaultBranchRef']['target']['history']['totalCount']
+                print(total_count, name)
+                overall_commits += int(total_count)
+
+        return overall_commits
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from get_data import GetData
 from edit_banner import EditBanner
 
 # TODO: get values from graphql
-test1 = GetData.test()
-print(test1)
+get_data = GetData()
+overall_commits = get_data.get_overall_commits()
+
+print(f"\nOverall Commits: {overall_commits}")
 
 # TODO: edit banner svg files
 EditBanner.test()
