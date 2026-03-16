590a895bf89b5f83f92452d4d26bc39dfb0f78f9
IMO <180146096+IMOitself@users.noreply.github.com>
2026-03-16 09:27:52 +0800

    fix(scripts): recent repo last update date is has no spacing;
    e.g March 152pm

--- edit_banner.py
+++ edit_banner.py
@@ -117,11 +117,11 @@ class EditBanner:
         today = datetime.now().astimezone().strftime("%B %d")
 
         display_date = last_update_date # ex. March 15
-        if(last_update_date == today): display_date = "Today at "
+        if(last_update_date == today): display_date = "Today at"
 
         hour = int(converted_date.strftime("%I"))
         am_pm = converted_date.strftime("%p").lower()
-        display_date = display_date + str(hour) + am_pm
+        display_date = display_date + " " + str(hour) + am_pm
 
         regex_pattern = r'(<div class="repo-updated-at">)[\s\S]*?(</div>)'
         replacement = rf'\g<1>{display_date}\g<2>'
