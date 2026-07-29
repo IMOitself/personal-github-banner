b56c721bb47df297659e0167341243edffc75a64
IMOitself <180146096+IMOitself@users.noreply.github.com>
2026-07-29 16:36:59 +0800

    feat(scripts): generate small repo banner;
    to be used later

--- make_banner_repo.py
+++ make_banner_repo.py
@@ -13,10 +13,59 @@ class MakeBannerRepo:
         new_file_content = re.sub(regex_pattern, replacement, new_file_content)
         # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
-# '<foreignObject xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="680" height="230">'
+# '<foreignObject x="0" y="0" width="680" height="230">'
         # change banner width
         regex_pattern = r'(<foreignObject[\s\S]*?width=")[\s\S]*?("[\s\S]*?>)'
         replacement = rf'\g<1>340\g<2>'
+        new_file_content = re.sub(regex_pattern, replacement, new_file_content)
+        # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+# <div class="sparkline-graph">...</div>'
+        # remove sparkline graph
+        regex_pattern = r'(<div class="sparkline-graph")[\s\S]*?(>[\s\S]*?</div>)'
+        replacement = rf'\g<1> style="display: none;"\g<2>'
+        new_file_content = re.sub(regex_pattern, replacement, new_file_content)
+        # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+# <div class="repo-updated-at">...</div>'
+        # remove 'last updated at ....'
+        regex_pattern = r'(<div class="repo-updated-at")[\s\S]*?(>[\s\S]*?</div>)'
+        replacement = rf'\g<1> style="display: none;"\g<2>'
+        new_file_content = re.sub(regex_pattern, replacement, new_file_content)
+        # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+# <div class="repo-body">...</div>'
+        # remove repo-body margin
+        regex_pattern = r'(<div class="repo-body")[\s\S]*?(>[\s\S]*?</div>)'
+        replacement = rf'\g<1> style="margin: 0"\g<2>'
+        new_file_content = re.sub(regex_pattern, replacement, new_file_content)
+        # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+# <div class="repo-card">...</div>'
+        # remove repo-card padding
+        regex_pattern = r'(<div class="repo-card")[\s\S]*?(>[\s\S]*?</div>)'
+        replacement = rf'\g<1> style="margin: 0"\g<2>'
+        new_file_content = re.sub(regex_pattern, replacement, new_file_content)
+        # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+# <div class="banner">...</div>'
+        # smaller banner padding
+        regex_pattern = r'(<div class="banner")[\s\S]*?(>[\s\S]*?</div>)'
+        replacement = rf'\g<1> style="padding: 16px"\g<2>'
+        new_file_content = re.sub(regex_pattern, replacement, new_file_content)
+        # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+# <div class="repo-is-archive" style="">Public archive</div>
+        # shorten is archive status instead of 'Public archive'
+        regex_pattern = r'(<div class="repo-is-archive"[\s\S]*?>)[\s\S]*?(</div>)'
+        replacement = rf'\g<1> archive \g<2>'
+        new_file_content = re.sub(regex_pattern, replacement, new_file_content)
+        # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+
+# <div class="repo-is-archive" style="">Public archive</div>
+        # shorten is archive status instead of 'Public archive'
+        regex_pattern = r'(<div class="repo-is-archive"[\s\S]*?>)[\s\S]*?(</div>)'
+        replacement = rf'\g<1> archive \g<2>'
         new_file_content = re.sub(regex_pattern, replacement, new_file_content)
         # -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
 
