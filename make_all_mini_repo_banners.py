4083cae8b5ef6944392da9da9825185a9a826db2
IMOitself <180146096+IMOitself@users.noreply.github.com>
2026-08-03 22:28:53 +0800

    feat: use short repo name on repo user created on mini repo banners

--- make_all_mini_repo_banners.py
+++ make_all_mini_repo_banners.py
@@ -12,25 +12,31 @@ def generate_mini_repo_banners():
         ('graphql/commits-on-other-repo.graphql', 'repositoriesContributedTo')
     ]
 
+    target_graphql_index = 0
+
     for graphql_path, repo_tag in target_graphql_strings:
         query = Path(graphql_path).read_text()
         repos = GetData.query_graphql(query, {"viewerId": GetData.viewerId})['data']['viewer'][repo_tag]['nodes']
 
         for repo in repos:
             reference_svg = Path('mini-repo-banners/base.svg').read_text(encoding='utf-8')
-            repo_banner_path = f'mini-repo-banners/{repo['nameWithOwner']}.svg'
-            Path(repo_banner_path).parent.mkdir(parents=True, exist_ok=True)
-            Path(repo_banner_path).write_text(reference_svg, encoding='utf-8')
 
+            try: repo['name']
+            except: repo['name'] = repo['nameWithOwner']
             if(repo['description'] is None): repo['description'] = "<i>No description.</i>"
             repo['commitCount'] = repo['defaultBranchRef']['target']['historyCommitCount']['totalCount']
 
-            EditBanner.change_recent_repo_name(repo_banner_path, repo['nameWithOwner'])
+            repo_banner_path = f'mini-repo-banners/{repo['name']}.svg'
+            Path(repo_banner_path).parent.mkdir(parents=True, exist_ok=True)
+            Path(repo_banner_path).write_text(reference_svg, encoding='utf-8')
+
+            EditBanner.change_recent_repo_name(repo_banner_path, repo['name'])
             EditBanner.change_recent_repo_description(repo_banner_path, repo['description'])
             EditBanner.change_recent_repo_language(repo_banner_path, repo['primaryLanguage'])
             EditBanner.change_recent_repo_is_archive(repo_banner_path, repo['isArchived'])
             EditBanner.change_recent_repo_commit_count(repo_banner_path, repo['commitCount'])
 
+        target_graphql_index += 1
 
 # TODO: only have the repo name to <repo-owner>/<repo-name> if its not the user's repo
 
