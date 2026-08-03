6a3db2b15a1a39b8a8a49a5ba3d339414f24e127
IMOitself <180146096+IMOitself@users.noreply.github.com>
2026-08-03 21:38:50 +0800

    test: actually turn graphql file response to json

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from get_data import GetData
 from pathlib import Path
+import json
 
 GetData = GetData()
-output = GetData.query_graphql(Path('graphql/recent_repo.graphql').read_text(), {"viewerId": GetData.viewerId})
-Path("test.json").write_text(str(output), encoding='utf-8')
+output = GetData.query_graphql(Path('graphql/commits-on-own-repo.graphql').read_text(), {"viewerId": GetData.viewerId})
+Path("test.json").write_text(json.dumps(output), encoding='utf-8')
 
 
 print("\n\n")
