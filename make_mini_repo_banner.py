c3ffd439962312fc14342038cb25cb6d654bce0d
IMOitself <180146096+IMOitself@users.noreply.github.com>
2026-08-16 01:35:25 +0800

    fix: most recent repo variables changes base mini repo banner everytime;
    which updates base, which updates all mini repo banners. messing up git history.
    
    feat: update mini repo banners

--- make_mini_repo_banner.py
+++ make_mini_repo_banner.py
@@ -1,5 +1,6 @@
 import re
 from pathlib import Path
+from edit_banner import EditBanner
 
 class MakeMiniRepoBanner:
     base_svg_path = 'mini-repo-banners/base.svg'
@@ -110,3 +111,9 @@ class MakeMiniRepoBanner:
 
         Path(self.base_svg_path).parent.mkdir(parents=True, exist_ok=True)
         Path(self.base_svg_path).write_text(new_file_content, encoding='utf-8')
+
+        EditBanner.change_recent_repo_name(self.base_svg_path, 'dummy repo name')
+        EditBanner.change_recent_repo_description(self.base_svg_path, 'dummy long ahh repo description that should be waaaaaaay too long idk')
+        EditBanner.change_recent_repo_language(self.base_svg_path, {'name': 'lolcat', 'color': "#D5B4AA"})
+        EditBanner.change_recent_repo_is_archive(self.base_svg_path, True)
+        EditBanner.change_recent_repo_commit_count(self.base_svg_path, 67)
